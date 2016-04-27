8ef767d2578dd7461f5165454a3118040316c261
RangelZZZ <18792561236@163.com>
2016-04-27 14:49:52 +0800

    perfect

--- test/main-test.js
+++ test/main-test.js
@@ -1,19 +1,46 @@
 describe(' integration testing', function () {
 
     var lcdDigits = loadLcdDigits();
-    var firstnumber= 910;
 
-    it('should print correct  number digits', function () {
+    it('should print correct  number digits when input has three numbers', function () {
+
+        var firstNumber = 910;
 
         spyOn(console, 'log');
 
-        showLcdDigits(firstnumber,lcdDigits);
+        showLcdDigits(firstNumber, lcdDigits);
 
-        var testNumberDigitsText = '\n' + '._.' + ' ' + '...' + ' ' + '._.' + ' ' + '\n' + '|_|' + ' ' + '..|' + ' ' + '|.|'
-            + ' ' + '\n' + '..|' + ' ' + '..|' + ' ' + '|_|' + ' ' + '\n';
+        var testNumberDigitsText = '\n' + '._. ... ._. ' + '\n' + '|_| ..| |.| ' + '\n' + '..| ..| |_| ' + '\n';
         expect(console.log).toHaveBeenCalledWith(testNumberDigitsText);
 
     });
+
+    it('should print correct  number digits when input has two numbers', function () {
+
+        var secondNumber = 65;
+
+        spyOn(console, 'log');
+
+        showLcdDigits(secondNumber, lcdDigits);
+
+        var testNumberDigitsText = '\n' + '._. ._. ' + '\n' + '|_. |_. ' + '\n' + '|_| ._| ' + '\n';
+        expect(console.log).toHaveBeenCalledWith(testNumberDigitsText);
+
+    });
+
+    it('should print correct  number digits when input has only one  numbers', function () {
+
+        var thirdNumber = 1;
+
+        spyOn(console, 'log');
+
+        showLcdDigits(thirdNumber, lcdDigits);
+
+        var testNumberDigitsText = '\n' + '... ' + '\n' + '..| ' + '\n' + '..| ' + '\n';
+        expect(console.log).toHaveBeenCalledWith(testNumberDigitsText);
+
+    });
+
 });
 
 
@@ -22,10 +49,10 @@ describe('unit testing', function () {
     describe('getNumberDigits', function () {
 
         var lcdDigits = loadLcdDigits();
-        var firstInteger = 910;
 
-        it('should print correct numberDigits  ', function () {
+        it('should print correct numberDigits when input has three numbers ', function () {
 
+            var firstInteger = 910;
             var testNumberDigits = [
                 ['._.', '|_|', '..|'],
                 ['...', '..|', '..|'],
@@ -37,27 +64,64 @@ describe('unit testing', function () {
 
         });
 
+        it('should print correct numberDigits when input has two numbers', function () {
+
+            var secondInteger = 65;
+            var testNumberdigits = [
+                ['._.', '|_.', '|_|'],
+                ['._.', '|_.', '._|']];
+
+            var numberDigits = getNumberDigits(secondInteger, lcdDigits);
+
+            expect(numberDigits).toEqual(testNumberdigits);
+
+        });
+
+        it('should print correct numberDigits when input has only one number', function () {
+
+            var thirdInteger = 1;
+            var testNumberdigits = [['...', '..|', '..|']];
+            var numberDigits = getNumberDigits(thirdInteger, lcdDigits);
+
+            expect(numberDigits).toEqual(testNumberdigits);
+
+        });
 
     });
 
-
     describe('printNumberDigits', function () {
 
-        var firstNumberDigits =
-            [['._.', '|_|', '..|'],
+        it('should print correct NumberDigitsText when input has three numers', function () {
+
+            var firstNumberDigits = [['._.', '|_|', '..|'],
                 ['...', '..|', '..|'],
                 ['._.', '|.|', '|_|']];
-
-
-        it('should print correct NumberDigitsText ', function () {
-
-            var testNumberDigitsText = '\n' + '._.' + ' ' + '...' + ' ' + '._.' + ' ' + '\n' + '|_|' + ' ' + '..|' + ' ' + '|.|' + ' ' + '\n' + '..|' + ' ' + '..|' + ' ' + '|_|' + ' ' + '\n';
+            var testNumberDigitsText = '\n' + '._. ... ._. ' + '\n' + '|_| ..| |.| ' + '\n' + '..| ..| |_| ' + '\n';
             var numberDigitsText = printNumberDigits(firstNumberDigits);
 
             expect(numberDigitsText).toEqual(testNumberDigitsText);
 
         });
 
+        it('should print correct NumberDigitsText when input has two numbers', function () {
+
+            var secondNumberDigits = [['._.', '|_.', '|_|'],
+                                      ['._.', '|_.', '._|']];
+            var testNumberDigitsText = '\n' + '._. ._. ' + '\n' + '|_. |_. ' + '\n' + '|_| ._| ' + '\n';
+            var numberDigitsText = printNumberDigits(secondNumberDigits);
+
+            expect(numberDigitsText).toEqual(testNumberDigitsText);
+
+        });
+
+        it('should print correct NumberDigitsText when input has only one numbers',function(){
+
+            var thirdNumberDigits = [['...', '..|', '..|']];
+            var testNumberDigitsText = '\n' + '... ' + '\n' + '..| ' + '\n' + '..| ' + '\n';
+            var numberDigitsText = printNumberDigits(thirdNumberDigits);
+
+            expect(numberDigitsText).toEqual(testNumberDigitsText);
+        });
     });
 });
 
