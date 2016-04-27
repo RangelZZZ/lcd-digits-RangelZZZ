51facac4e0ff5776a398aeb5e4f320c1cd52041c
RangelZZZ <18792561236@163.com>
2016-04-27 13:10:15 +0800

    test

--- test/main-test.js
+++ test/main-test.js
@@ -1,12 +1,13 @@
 describe(' integration testing', function () {
 
     var lcdDigits = loadLcdDigits();
+    var firstnumber= 910;
 
     it('should print correct  number digits', function () {
 
         spyOn(console, 'log');
 
-        showLcdDigits(lcdDigits);
+        showLcdDigits(firstnumber,lcdDigits);
 
         var testNumberDigitsText = '\n' + '._.' + ' ' + '...' + ' ' + '._.' + ' ' + '\n' + '|_|' + ' ' + '..|' + ' ' + '|.|'
             + ' ' + '\n' + '..|' + ' ' + '..|' + ' ' + '|_|' + ' ' + '\n';
@@ -20,41 +21,22 @@ describe('unit testing', function () {
 
     describe('getNumberDigits', function () {
 
-        var lcdDigits =loadLcdDigits();
+        var lcdDigits = loadLcdDigits();
         var firstInteger = 910;
-        var secondInteger = 65;
-        var ThirdInteger = 4;
 
-        it('should print correct numberDigits when the integer has three numbers ', function () {
+        it('should print correct numberDigits  ', function () {
 
             var testNumberDigits = [
-                 ['._.', '|_|', '..|'],
-                 ['...', '..|', '..|'],
-                 ['._.', '|.|', '|_|']];
+                ['._.', '|_|', '..|'],
+                ['...', '..|', '..|'],
+                ['._.', '|.|', '|_|']];
+
             var numberDigits = getNumberDigits(firstInteger, lcdDigits);
 
             expect(numberDigits).toEqual(testNumberDigits);
 
         });
 
-        it('should print correct numberDigits when the integer has two numbers', function () {
-
-            var testNumberDigits = [
-                 ['._.', '|_.', '|_|'],
-                 ['._.', '|_.', '._|']];
-            var numberDigits = getNumberDigits(secondInteger, lcdDigits);
-
-            expect(numberDigits).toEqual(testNumberDigits);
-        });
-
-
-        it('should print correct numberDigits when the integet has one numbers', function () {
-
-            var testNumberDigits = [['._.', '._|', '._|']];
-            var numberDigits = getNumberDigits(ThirdInteger, lcdDigits);
-
-            expect(numberDigits).toEqual(testNumberDigits);
-        });
 
     });
 
@@ -62,46 +44,20 @@ describe('unit testing', function () {
     describe('printNumberDigits', function () {
 
         var firstNumberDigits =
-            [ ['._.', '|_|', '..|'],
-            ['...', '..|', '..|'],
-            ['._.', '|.|', '|_|']];
+            [['._.', '|_|', '..|'],
+                ['...', '..|', '..|'],
+                ['._.', '|.|', '|_|']];
 
-        var secondNumberDigits = [
-             ['._.', '|_.', '|_|'],
-             ['._.', '|_.', '._|']];
 
-        var thirdNumberDigits = [ ['._.', '._|', '._|']];
+        it('should print correct NumberDigitsText ', function () {
 
-        it('should print correct NumberDigitsText when we need print three numbers ', function () {
-
-            var testNumberDigitsText = '\n' + '._.' + ' ' + '...' + ' ' + '._.' + ' ' + '\n' + '|_|' + ' ' + '..|' + ' ' + '|.|'
-                + ' ' + '\n' + '..|' + ' ' + '..|' + ' ' + '|_|' + ' ' + '\n';
+            var testNumberDigitsText = '\n' + '._.' + ' ' + '...' + ' ' + '._.' + ' ' + '\n' + '|_|' + ' ' + '..|' + ' ' + '|.|' + ' ' + '\n' + '..|' + ' ' + '..|' + ' ' + '|_|' + ' ' + '\n';
             var numberDigitsText = printNumberDigits(firstNumberDigits);
 
             expect(numberDigitsText).toEqual(testNumberDigitsText);
 
         });
 
-        it('should print correct NumberDigitsText when we need print two numbers ', function () {
-
-            var testNumberDigitsText = '\n' + '._.' + ' ' + '._.' +' '+ '\n' + '|_.' + ' ' + '|_.' +' ' +'\n' + '|_|' + ' ' + '._|' + ' ' + '\n';
-            var numberDigitsText = printNumberDigits(secondNumberDigits);
-
-            expect(numberDigitsText).toEqual(testNumberDigitsText);
-
-        });
-
-
-        it('should print correct NumberDigits when we need print one numbers', function () {
-
-            var testNumberDigitsText = '._.' + ' ' +'\n' + '._|'+ ' ' + '\n' + '._|' + ' ' + '\n';
-            var numberDigitsText = printNumberDigits(thirdNumberDigits);
-
-            expect(numberDigitsText).toEqual(testNumberDigitsText);
-
-        });
-
     });
-
 });
 
