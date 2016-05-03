1deb8fe4381ae5c9cfefce18a932faa4d7545fbf
RangelZZZ <18792561236@163.com>
2016-05-03 15:58:17 +0800

    1

--- main/main.js
+++ main/main.js
@@ -1,7 +1,6 @@
 function showLcdDigits(firstnumber, lcdDigits) {
 
     var numberDigits = getNumberDigits(firstnumber, lcdDigits);
-
     var numberDigitsText = printNumberDigits(numberDigits);
 
     console.log(numberDigitsText);
@@ -10,19 +9,15 @@ function showLcdDigits(firstnumber, lcdDigits) {
 
 function getNumberDigits(firstInteger, lcdDigits) {
 
-
     var numberDigits = [];
     var stringNumber = firstInteger.toString();
 
     for (var i = 0; i < stringNumber.length; i++) {
-
         if (isExit(parseInt(stringNumber.charAt(i)), lcdDigits)) {
-
             var digit = isExit(parseInt(stringNumber.charAt(i)), lcdDigits);
             numberDigits.push(digit);
         }
     }
-
     return numberDigits;
 }
 
@@ -31,7 +26,6 @@ function isExit(number, lcdDigits) {
 
     for (var i = 0; i < lcdDigits.length; i++) {
         if (number === parseInt(lcdDigits[i].number)) {
-
             return lcdDigits[i].digits;
         }
     }
@@ -41,11 +35,11 @@ function isExit(number, lcdDigits) {
 function printNumberDigits(numberDigits) {
 
     var numberDigistsText = '\n';
+
     for (var j = 0; j < 3; j++) {
         for (var i = 0; i < numberDigits.length; i++) {
             numberDigistsText += numberDigits[i][j] + ' ';
         }
-
         numberDigistsText += '\n';
     }
     return numberDigistsText;
